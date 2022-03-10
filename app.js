8117334f067f1458def9b09f0bed6ac94d562b24
Erzhan <aldamatov@gmail.com>
2022-03-11 10:54:38 +1100

    Read Books Added

--- app.js
+++ app.js
@@ -37,3 +37,43 @@ for ( let i = 1; i <= topChoice.length; i ++){
     console.log(`My ${i}th choice is ${topChoice[i-1]}`)
   }
 }
+
+
+//The Reading List
+//Keep track of which books you read and which books you want to read!
+
+//Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+//Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+//Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+console.log(" ")
+console.log("== Read Books ==")
+
+let books = [
+    {
+      title: 'The Hobbit',
+      author: 'J.R.R. Tolkien',
+      alreadyRead: true
+    },
+    {
+      title: 'Sherlock Holms',
+      author: 'Arthur Conan Doyle',
+      alreadyRead: false
+    },
+    {
+      title: 'Eloquent JavaScript',
+      author: 'Marijn Haverbeke',
+      alreadyRead: true
+    }
+  ]
+  for (let i = 0; i < books.length; i++){
+    //console.log(books[i].title + " by " + books[i].author)
+    if (books[i].alreadyRead === true){
+      console.log("You have already read " + books[i].title +" by " + books[i].author);
+      
+    }
+    else {
+      console.log("You need to read " + books[i].title + " by " + books[i].author);
+    }
+  }
+  
